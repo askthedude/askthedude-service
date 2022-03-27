5cfed94e6361f3786a94ac7ef40d3975345afb93
nikasakana <nikasakana@gmail.com>
2022-03-27 21:17:12 +0400

    added first routers of fast api for users feature

---
+++ service/app/web/user.py
@@ -0,0 +1,32 @@
+from fastapi import APIRouter
+from pydantic import BaseModel
+from dataclasses import dataclass
+
+router = APIRouter()
+
+
+@dataclass
+class GetUser(BaseModel):
+    id: int
+    name: str
+    surname: str
+    email: str
+    is_active: bool
+
+
+@dataclass
+class PostUser(BaseModel):
+    name: str
+    surname: str
+    email: str
+    password: str
+
+
+@router.get("/user/{id}", response_model=GetUser)
+async def get_user(id: str):
+    return GetUser(1, 'nika', 'sakana', 'email.com', True)
+
+
+@router.post("/user/", response_model=GetUser)
+async def add_user(user: PostUser):
+    return GetUser(1, 'nika', 'sakana', 'email.com', True)
