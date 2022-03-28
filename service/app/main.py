9878c6201086cc0ceaf29c3193a7c92e83ca72e5
nikasakana <nikasakana@gmail.com>
2022-03-28 23:08:52 +0400

    added logging middleware'

--- service/app/main.py
+++ service/app/main.py
@@ -1,6 +1,22 @@
-from fastapi import FastAPI
-import web.controller as router
 
+from fastapi import FastAPI
+from starlette.requests import Request
+
+import web.controller as router
+from utils.log_config import LogConfig
+from logging.config import dictConfig
+import logging
+
+dictConfig(LogConfig().dict())
 app = FastAPI()
+
+logger = logging.getLogger("WEBAPI")
+
+
+@app.middleware("http")
+async def logger_middleware(request: Request, call_next):
+    logger.info(f"Got HTTP {request.method} Request on URI: {request.url.path}. {request.body()}.")
+    response = await call_next(request)
+    return response
 
 app.include_router(router.router)
