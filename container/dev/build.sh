042b6c4fa0c970e129f0d439b7956565776df808
nikasakana <nikasakana@gmail.com>
2022-03-28 22:45:06 +0400

    minor updated to build process

--- container/dev/build.sh
+++ container/dev/build.sh
@@ -6,9 +6,10 @@ then
     docker-compose -f dev-docker-compose.yml up -d --build --force-recreate
 elif [ $SERVICE = "storage" ]
 then
+    echo "Task: Restart storage container"
     if [ -z "$FLAG"  ]
     then
-        docker-compose -f dev-docker-compose.yml up -d storage --build --force-recreate
+        docker-compose -f dev-docker-compose.yml up -d  --build --force-recreate storage
     elif [ $FLAG = "--drop" ]
     then
         echo "stopping container: dev_sotrage_1"
@@ -19,8 +20,19 @@ then
         docker volume rm dev_storage
         echo "starting up new storage container"
         docker-compose -f dev-docker-compose.yml up -d --build --force-recreate storage
-        
     else
         echo "CAKE"
     fi
+elif [ $SERVICE = "api" ]
+then
+  echo "Task: Restart api container"
+  echo "removing container: api"
+  docker container stop api
+  docker container rm api
+  docker-compose -f dev-docker-compose.yml up  -d --build --force-recreate api
+  echo "Restarted container: api"
+elif [ $SERVICE = "help" ]
+then
+  echo "===============help==============="
+
 fi
