c4c12419c987376658f9ff221e498c232adf7c90
nikasakana <nikasakana@gmail.com>
2022-04-25 19:45:00 +0400

    minor refactoring

--- service/app/service/validation/validation.py
+++ service/app/service/validation/validation.py
@@ -1,37 +1,11 @@
 from typing import List
 
+from service.validation.utils import VALIDATION_ERROR_MESSAGES, regex, PASSWORD_MIN_LENGTH
 from storage.facade.project_storage_facade import filter_technologies, get_project_by_id
 from web.dto.dto import PostUser, SignInUser, PostProject, TechnologyFilter, ProjectSubscriptionData, \
     PostStatistics, PostRole, AnonymousUserData, PostTechnology
 from dataclasses import dataclass, field
 import re
-
-regex = re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+')
-
-VALIDATION_ERROR_MESSAGES = {
-    "username": "Username is missing.",
-    "email": "Email is missing.",
-    "email_format": "Email format is not correct.",
-    "name": "Name is missing.",
-    "password": "Password is missing.",
-    "password_strength": "Password too short. Please choose password of at least of length 5. )",
-    "title": "Project title is missing",
-    "technologies": "Technology id-s are missing for the project.",
-    "technology_ids_invalid": "Specified technology ids are not valid",
-    "description": "Project description is missing",
-    "start_date": "Project start-date is missing",
-    "project": "Project with specified id is missing",
-    "project_missing": "Project id is missing from the subscription",
-    "subscriptions": "Number of subscription change is not valid",
-    "seen_frequency": "Number of seen frequency change is not valid",
-    "interested_num": "Number of interested people in project not valid",
-    "role": "Role title not specified",
-    "identifier_token": "identifier_token for anonymous user not present.",
-    "technology_name": "Technology name is missing",
-    "technology_url": "Technology url is missing"
-}
-
-PASSWORD_MIN_LENGTH = 5
 
 
 @dataclass
