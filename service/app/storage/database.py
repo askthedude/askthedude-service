d2a05b7b7eda790d034761b278ab1ef490047f2b
nikasakana <nikasakana@gmail.com>
2022-05-22 19:36:17 +0400

    added comments

--- service/app/storage/database.py
+++ service/app/storage/database.py
@@ -4,11 +4,13 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.asyncio import AsyncSession
 from dependencies.config import database_url, settings
 
+# async storage engine/driver using database url
 engine = create_async_engine(
     database_url,
     echo=True if settings.development_mode else False
 )
 
+# initialize new session maker/session factory using async engine define above
 new_session = sessionmaker(engine, autocommit=False, autoflush=False, class_=AsyncSession, expire_on_commit=False)
 
 Base = declarative_base()
